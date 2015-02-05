c2c81989538466d28eaeb2951af5ea0cec6b394c
Robert Dickerson <robert@aurovo.com>
2015-02-04 23:51:49 -0500

    Updated with marker and infowindows

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,6 @@
 //Global map variable
 var map;
 
-//Get the location to display the coordinates
-var lat = document.getElementById("latcoords");
-var lng = document.getElementById("loncoords");
-
 //Function run on DOM load
 function loadMap() {
     
@@ -16,39 +12,7 @@ function loadMap() {
 
         //Map center
         center: new google.maps.LatLng(40.748817,-73.985428),
-        
-        //Limit min/max zoom
-        minZoom: 2,
-        maxZoom: 18,
-
-        mapTypeControl: true,
-        mapTypeControlOptions: {
-            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP,
-                         google.maps.MapTypeId.SATELLITE,
-                         google.maps.MapTypeId.HYBRID,     
-                         google.maps.MapTypeId.TERRAIN]                   
-        },
-        mapTypeId: google.maps.MapTypeId.TERRAIN,       
-
-        //0 to 45deg, only valid for satellite and terrain
-        tilt: 0,
-
-        zoomControl: true,
-        zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.SMALL,
-            position: google.maps.ControlPosition.RIGHT_TOP
-        },
-
-        panControl: true,
-        streetViewControl: true,
-
-        //Overview map
-        overviewMapControl: true,
-        overviewMapControlOptions: {
-            opened: true
-        },
-
+      
         //Set the map style
         styles: shiftWorkerMapStyle, 
     };
@@ -58,57 +22,62 @@ function loadMap() {
 
     //Create the map
     map = new google.maps.Map(mapid,mapOptions);
-    
-    //Update the lat/lng on load of the map center
-    updateCurrentLatLng(map.getCenter());
-          
-    //Add the event listeners
-    mapEventListeners();
-       
 
-}
-
-// Add the map event listeners
-function mapEventListeners() {
-    
-    // Mouse move updates the coordinates 
-    var mouseMoveChanged = google.maps.event.addListener(map, 'mousemove',
-        function(event) {            
-        
-            //Update the coordinates
-            updateCurrentLatLng(event.latLng);
-            
-        }
-    );
-    
-            //Wait for map to load
-//        var listenerIdle = google.maps.event.addListenerOnce(map, 'idle',
-//            function() {
-//                //TODO
-//            }
-//        );
-//        
-//        var listenerDragEnd = google.maps.event.addListener(map, 'dragend', 
-//            function() {
-//                //TODO
-//            }
-//        );
-//
-//        var listenerZoomChanged = google.maps.event.addListener(map, 'zoom_changed',
-//            function() {
-//                //TODO
-//            }
-//        );
+    //Marker creation
+    var newMarker = this.addMarker();
+    addInfoWindow(newMarker);
     
 }
 
-// Update the position of the mouse in latitude and longitude
-function updateCurrentLatLng(latLng) {
+//Add a marker to the map
+function addMarker() {
+     
+    //Create the marker (#MarkerOptions)    
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(40.748817,-73.985428),
+        map: map,        
+        title: 'Tooltip Title',
+//        icon : new google.maps.MarkerImage(icon,
+//                    new google.maps.Size(32,32),
+//                    new google.maps.Point(0,0),
+//                    new google.maps.Point(0,32),
+//                    new google.maps.Size(24,24)) 
+        //zIndex
+        //visible
+        //keep multiple pop-ups open
+    });
 
-    //Update the coordinates
-    lat.innerHTML = latLng.lat();
-    lng.innerHTML = latLng.lng();
+    marker.myid = "myuniquemarker";
     
+        //Marker events (#MarkerEvents)
+    //    marker.setMap(map);
+    //    marker.setVisible(false);
+    //marker.setOptions({draggable: true });
+
+    return marker;
+}
+
+function addInfoWindow(marker) {
+   
+    //Add embedded image and text with link _blank
+    //Add javascript 
+    var contentString = '<div class="infowindowcontent">'+
+        '<h1>Title</h1>'+
+        '<div class="infowindowaddress">Address</div>'+
+        '<input type="button" value="Submit" onclick="submitHandler()">' +
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+    });
+}
+
+function submitHandler() {
+    alert('Form Submitted for Marker');
 }
 
 //Load the map
