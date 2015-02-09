916171ab919a9ab0bff1395523926d295e48cbd2
Robert Dickerson <robert@aurovo.com>
2015-02-09 00:32:20 -0500

    Added custom icon

--- js/scripts.js
+++ js/scripts.js
@@ -25,59 +25,41 @@ function loadMap() {
 
     //Marker creation
     var newMarker = this.addMarker();
-    addInfoWindow(newMarker);
-    
+        
 }
 
 //Add a marker to the map
 function addMarker() {
-     
+        
     //Create the marker (#MarkerOptions)    
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(40.748817,-73.985428),
-        map: map,        
-        title: 'Tooltip Title',
-//        icon : new google.maps.MarkerImage(icon,
-//                    new google.maps.Size(32,32),
-//                    new google.maps.Point(0,0),
-//                    new google.maps.Point(0,32),
-//                    new google.maps.Size(24,24)) 
-        //zIndex
-        //visible
-        //keep multiple pop-ups open
+        map: map,                
+        icon: {
+            
+            //URL of the image
+            url: 'img/custom_icon.png',
+            
+            //Sets the image size
+            size: new google.maps.Size(32,32),
+            
+            //Sets the origin of the image (top left)
+            origin: new google.maps.Point(0,0),
+            
+            //Sets the anchor (middle, bottom)
+            anchor: new google.maps.Point(16,32),
+            
+            //Scales the image
+            scaledSize: new google.maps.Size(32,32)
+        }
     });
 
-    marker.myid = "myuniquemarker";
-    
-        //Marker events (#MarkerEvents)
-    //    marker.setMap(map);
-    //    marker.setVisible(false);
-    //marker.setOptions({draggable: true });
+  
+    //Marker events (#MarkerEvents)
+//    marker.setMap(map);
+//    marker.setVisible(false);
 
     return marker;
-}
-
-function addInfoWindow(marker) {
-   
-    //Add embedded image and text with link _blank
-    //Add javascript 
-    var contentString = '<div class="infowindowcontent">'+
-        '<h1>Title</h1>'+
-        '<div class="infowindowaddress">Address</div>'+
-        '<input type="button" value="Submit" onclick="submitHandler()">' +
-        '</div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
-    });
-}
-
-function submitHandler() {
-    alert('Form Submitted for Marker');
 }
 
 //Load the map
